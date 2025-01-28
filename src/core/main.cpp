36ff44b19beb04adcd155cfc60240674ba019c17
Kamalesh Pandian <106421993+kamalesh003@users.noreply.github.com>
2025-01-28 22:45:08 +0530

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,28 +1,16 @@
 #include "cortix_processor.hpp"
 #include <iostream>
-#include <thread>
-#include <chrono>
 
 int main() {
     CortiXProcessor implant("PT-1138");
     while (true) {
         std::this_thread::sleep_for(std::chrono::hours(24));
-
-        try {
-            // Update patient data
-            implant.update_knowledge(
-                "INSERT DATA { "
-                "   <http://cortix/patient/PT-1138> :lastMeal :SpicyFood ; "
-                "                                    :currentStressLevel 68 . "
-                "}"
-            );
-
-            // Print success message
-            std::cout << "Success: Patient data updated for PT-1138!" << std::endl;
-        } catch (const std::exception& e) {
-            // Handle errors
-            std::cerr << "Error: " << e.what() << std::endl;
-        }
+        implant.update_knowledge(
+            "INSERT DATA { "
+            "   <http://cortix/patient/PT-1138> :lastMeal :SpicyFood ; "
+            "                                    :currentStressLevel 68 . "
+            "}"
+        );
     }
     return 0;
 }
